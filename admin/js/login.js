bdd638b5ff0039e4eb5bd72f825737d1dba21395
Yang-Ying-Zhuang <57862886+Yang-Ying-Zhuang@users.noreply.github.com>
2020-08-21 14:48:00 +0800

    02-完成登录页面

--- admin/js/login.js
+++ admin/js/login.js
@@ -4,23 +4,29 @@ $(function () {
     // 收集数据
     //  $("form").serislize():它可以收集指定form表单中 拥有name属性的表单元素的value值,生成key=value&key=value
     // console.log($("form").serialize());
-  
+
     $.ajax({
       type: "post",
       url: "http://localhost:8080/api/v1/admin/user/login",
-      data: $(".login_form").serialize(),
-      headers:token,
+      data: $("form").serialize(),
+      dataType: "json",
       success: (res) => {
-         console.log(res);
+        //  console.log(res);
         if (res.code === 200) {
-          alert("登录成功")
-            // localStorage.setItem("heima52",JSON.stringify("token"))
-          window.location.href = "./index.html"
-
+          $(".modal-body>p").text(res.msg)
+          $('#myModal').modal('show')
+          $('#myModal').on('hidden.bs.modal', function (e) {
+            window.location.href = "./index.html"
+          })
+        } else {
+          $(".modal-body>p").text(res.msg)
+          $('#myModal').modal('show')
         }
-      },
-      dataType: "json"
-
+      }
+    })
+    $(".btn-primary").on("click",function(){
+      $('#myModal').modal('hide')
     })
   })
+
 })
