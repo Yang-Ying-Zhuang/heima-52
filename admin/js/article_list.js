18c8e1bffc8afbad72c1e8704e8b9262f8223223
Yang-Ying-Zhuang <57862886+Yang-Ying-Zhuang@users.noreply.github.com>
2020-08-24 14:49:00 +0800

    完成筛选功能

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -1,5 +1,27 @@
 $(function () {
 
+   // 文章类别管理
+   function templa() {
+    $.ajax({
+      type: "get",
+      url: BigNew.category_list,
+      headers: {
+        Authorization: localStorage.getItem("token-52")
+      },
+      dataType: "json",
+      success: (res) => {
+        // console.log(res);
+        if (res.code === 200) {
+          const data = template("wenZhang", res)
+          $("#selCategory").html(data)
+        }
+      }
+    })
+  }
+  // 调用
+  templa();
+
+
   // #### 10、文章搜索
   // 请求地址：/admin/article/query
   // 请求方式：get
@@ -7,12 +29,14 @@ $(function () {
      $.ajax({
        type:"get",
        url:BigNew.article_query,
-       data:{},
+       data:{
+        // perpage:10,
+       },
        dataType:"json",
        success:(res)=>{
         //  console.log(res);
         if(res.code === 200){
-          const data = template("timp",res.data)
+          const data = template("temp",res.data)
           $('tbody').html(data)
         }
        }
